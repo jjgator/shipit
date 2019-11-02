00370b50443ccb4cd3fcaa6c7619e5ebf5199949
Julie Johnson <jjgator417@gmail.com>
2019-11-02 17:40:04 -0500

    add thunk and restructure src folder

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from './reducers/rootReducer';
 import './index.css';
-import App from './App/App';
+import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 
 const store = createStore(rootReducer);
