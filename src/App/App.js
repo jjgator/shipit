46b8445013958dd210ab77bf81f880f9446ebfc1
Julie Johnson <jjgator417@gmail.com>
2019-11-01 22:53:25 -0500

    initial redux config

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
     return (
         <header className="header">
             <p>Ship It!</p>
         </header>
     );
-}
+};
 
 export default App;
