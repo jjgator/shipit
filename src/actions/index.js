85b71fcc68b6b8a2d7d178bae2575c9b5f77c009
Julie Johnson <jjgator417@gmail.com>
2019-11-02 20:54:39 -0500

    api validation of address

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,31 @@
 let nextStopId = 0;
 
-const addStop = (name, address) => ({
-    type: 'ADD_STOP',
-    id: nextStopId++,
-    name,
-    address
-});
+const addStop = (name, address) => async(dispatch) => {
+    try {
+        let data = await validateAddress(address);
 
-const validateAddress = (address) => dispatch => {
+        dispatch({
+            type: 'ADD_STOP',
+            id: nextStopId++,
+            name,
+            address: data.geocoded_address.formatted_address
+        });
+    } catch (error) {
+        alert("This address could not be validated. Please enter a valid address.")
+    }
+};
 
+const validateAddress = async(address) => {
+    let url = "https://dev-api.shipwell.com/v2/locations/addresses/validate/";
+    let data = { formatted_address: address };
+
+    let response = await fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    });
+
+    return await response.json();
 };
 
 export { addStop };
